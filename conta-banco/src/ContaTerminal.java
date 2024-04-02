50b1c81a0538f7d0c9cc7e40e0928d1e03d8aeda
Gabriel Müller <gabrielmuller30.gml@gmail.com>
2024-04-02 14:49:48 -0300

    att processos

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,23 +1,32 @@
 import java.util.Scanner; //Espaço destinado para implementação de bibliotecas
+import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner (System.in); //Método construtor do scanner para inserção de dados 
-        
-        System.out.println("Olá, vamos fazer o login de sua conta bancária:");
+        scanner.useLocale(Locale.US); //Definindo a localidade para usar o ponto como separador.
 
-        System.out.println(" Insira o número de seu usuário.");
+        System.out.println("Olá, vamos fazer o login de sua conta bancária.\n");
+
+        System.out.println("Insira o número de seu usuário.");
         int numero = scanner.nextInt();
-        System.out.println("\nUsuário: " + numero);
+        System.out.println("Usuário: " + numero);
         
         System.out.println("\nPor favor, digite o número de sua agência.");
-        String agencia = scanner.next();
-        System.out.println("\nAgência: " + agencia);
+        String agencia = scanner.next();//Tem como usar o nextLine(), porém, eu ainda não sei exatamente como usar, apenas que ele capta os dados de uma linha até a quebra de linha.
+        System.out.println("Agência: " + agencia);
+
+        System.out.println("\nAgora, insira o seu nome.");
+        String nome_cliente = scanner.next();
+        System.out.println("Nome: "+ nome_cliente);
+
+        System.out.println("\nInsira o seu saldo: ");
+        double saldo = scanner.nextDouble();
+        System.out.println("Saldo: " + saldo);
         
-        String nome_cliente = "Gabriel Müller";
-        double saldo = 1581.77;
+
+        System.out.println("\nOlá " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque.");   
         
-        System.out.println("\nOlá " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-        + agencia + ", conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque.");   
+        scanner.close();//fechamento do scanner
     }
 }
